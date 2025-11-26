fb5651e482fd7b20606c5855552a170e5896252d
niteoowl <xx.niteowl@gmail.com>
2025-11-26 12:16:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,11 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-  // 기본적으로 네트워크 요청을 그대로 수행
-  e.respondWith(fetch(e.request));
-});
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker 등록 성공:', registration.scope);
+      })
+      .catch(err => {
+        console.log('Service Worker 등록 실패:', err);
+      });
+  });
+}
